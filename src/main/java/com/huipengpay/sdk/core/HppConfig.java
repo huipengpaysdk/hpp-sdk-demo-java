64c606ea46bf0de35c970474d75939c2715e35d7
atomic <atomicmm@gmail.com>
2015-09-30 14:57:27 +0800

    initial project

--- src/main/java/com/huipengpay/sdk/core/HppConfig.java
+++ src/main/java/com/huipengpay/sdk/core/HppConfig.java
@@ -12,13 +12,14 @@ public final class HppConfig {
     public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
     //默认的Post请求格式
     public static final String REQUEST_CONTENT_TYPE = "text/plain";
+
     //PC网关支付路径
-    public static final String URL_PC_GATEWAY_PAY = "https://cashier.huipengpay.com/api/v1/pay";
     //订单查询路径
+    public static final String URL_PC_GATEWAY_PAY = "https://cashier.huipengpay.com/api/v1/pay";
     public static final String URL_ORDER_QUERY = "https://cashier.huipengpay.com/api/v1/order-query";
 
 
-    public static String PASSPORT_KEY = "Hl0kp77WbCF8m0dF+IzOsQ==";
-    public static String PASSPORT_NUMBER = "HPPPN20157083500001";
 
+    public static String PASSPORT_KEY = "Hl0kp77WbCF8m0dF+IzOsQ==";//加密私钥
+    public static String PASSPORT_NUMBER = "HPPPN20157083500001";//商户号
 }
